8371350498a5d6ffe083c85584c7512b94207b4c
Widliwoo <shubinds@rambler.ru>
2022-08-09 14:02:19 +0300

    Заготовка для сервиса

---
+++ wow-auction-extractor/src/main/java/com/example/extractor/WowAuctionExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowAuctionExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowAuctionExtractorApplication.class, args);
+	}
+
+}
---
+++ wow-auction-extractor/src/test/java/com/example/extractor/WowAuctionExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowAuctionExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
